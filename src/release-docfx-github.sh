abbfee787550f4bad846b0f3ce9f1842a1133c8e
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-08-18 17:47:21 -0500

    Updated CI pipeline

--- src/release-docfx-github.sh
+++ src/release-docfx-github.sh
@@ -20,7 +20,7 @@ cp -r $SOURCE_DIR/../docs/_site/* .
 
 echo "Push the new docs to the remote branch"
 git config --global user.email ${DOCFX_GITHUB_EMAIL}
-git config --global user.name ByteDecoder
+git config --global user.name "ByteDecoder"
 
 git add . -A
 git commit -m "Update generated documentation"
