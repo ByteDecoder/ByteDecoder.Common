19509339a4c8fddb6f2ce23e3168d9c76a092646
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-08-18 16:38:50 -0500

    Updated CI pipelien

--- src/release-docfx-github.sh
+++ src/release-docfx-github.sh
@@ -5,8 +5,11 @@ SOURCE_DIR=$PWD/../docs
 TEMP_REPO_DIR=$PWD/../gh-pages
 
 echo "Removing temporary doc directory $TEMP_REPO_DIR"
-# rm -rf $TEMP_REPO_DIR
-# mkdir $TEMP_REPO_DIR
+rm -rf $TEMP_REPO_DIR
+mkdir $TEMP_REPO_DIR
+
+echo "Cloning the repo with the gh-pages branch"
+git clone https://${BYTEDECODER_COMMON_GITHUB_CI_ACTIONS):x-oauth-basicgithub.com/ByteDecoder/ByteDecoder.Common.git --branch gh-pages $TEMP_REPO_DIR
 
 echo "Clear repo directory"
 cd $TEMP_REPO_DIR
