c4f8318a3b83f267774d3067f4f939a490abad94
ByteDecoder <rreyes@canaryhealth.com>
2020-11-14 16:54:57 -0600

    Added string extensions common methods

---
+++ src/ByteDecoder.Common/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace ByteDecoder.Common
+{
+    /// <summary>
+    /// String extensions methods.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Convert a string to integer type.
+        /// </summary>
+        /// <param name="source">Source string.</param>
+        /// <returns>Parse result.</returns>
+        public static int ToInt(this string source)
+        {
+            return int.Parse(source, NumberFormatInfo.CurrentInfo);
+        }
+
+        /// <summary>
+        /// Convert a string to double type.
+        /// </summary>
+        /// <param name="source">Source string.</param>
+        /// <returns>Parse result.</returns>
+        public static double ToDouble(this string source)
+        {
+            return double.Parse(source, NumberFormatInfo.CurrentInfo);
+        }
+    }
+}
