d5b6a246b4617794d0047444d36a9e568ca11473
ByteDecoder <rreyes@canaryhealth.com>
2021-01-20 00:33:08 -0600

    Added Enum Type extensions methods for the String Type

---
+++ src/ByteDecoder.Common/EnumExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ByteDecoder.Common
+{
+    /// <summary>
+    /// Enum type extensions.
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Converts a string value to an Enum value.
+        /// </summary>
+        /// <typeparam name="T">Enum type.</typeparam>
+        /// <param name="value">Value to convert.</param>
+        /// <returns>Enum value.</returns>
+        public static T ToEnum<T>(this string value) where T : struct, IConvertible
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        /// <summary>
+        /// Converts a string value to an Enum value, if fails returns the default value
+        /// specified.
+        /// </summary>
+        /// <typeparam name="T">Enum type.</typeparam>
+        /// <param name="value">Value to convert.</param>
+        /// <param name="defaultValue">Default enum value.</param>
+        /// <returns>Enum value.</returns>
+        public static T ToEnum<T>(this string value, T defaultValue) where T : struct, IConvertible
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return defaultValue;
+
+            T result;
+            return Enum.TryParse<T>(value, true, out result) ? result : defaultValue;
+        }
+    }
+}
