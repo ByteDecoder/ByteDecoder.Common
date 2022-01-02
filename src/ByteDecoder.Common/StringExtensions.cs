9d78e0b2f24df6950be3ae5463cf601e8ebd0d77
ByteDecoder <rreyes@canaryhealth.com>
2022-01-02 15:09:02 -0600

    Code updated to C# 10 syntax

--- src/ByteDecoder.Common/StringExtensions.cs
+++ src/ByteDecoder.Common/StringExtensions.cs
@@ -1,53 +1,51 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 
-namespace ByteDecoder.Common
+namespace ByteDecoder.Common;
+
+/// <summary>
+/// String extensions methods.
+/// </summary>
+public static class StringExtensions
 {
     /// <summary>
-    /// String extensions methods.
+    /// Convert a string to integer type.
     /// </summary>
-    public static class StringExtensions
+    /// <param name="source">Source string.</param>
+    /// <returns>Parse result.</returns>
+    public static int ToInt(this string source)
     {
-        /// <summary>
-        /// Convert a string to integer type.
-        /// </summary>
-        /// <param name="source">Source string.</param>
-        /// <returns>Parse result.</returns>
-        public static int ToInt(this string source)
+        return int.Parse(source, NumberFormatInfo.CurrentInfo);
+    }
+
+    /// <summary>
+    /// Convert a string to double type.
+    /// </summary>
+    /// <param name="source">Source string.</param>
+    /// <returns>Parse result.</returns>
+    public static double ToDouble(this string source)
+    {
+        try
         {
-            return int.Parse(source, NumberFormatInfo.CurrentInfo);
+            return double.Parse(source, NumberFormatInfo.CurrentInfo);
+        }
+        catch (FormatException)
+        {
+            return double.NaN;
+        }
+    }
+
+    /// <summary>
+    /// Converts an string into a camelCase representation.
+    /// </summary>
+    /// <param name="source">Source string.</param>
+    /// <returns>Camel case string result.</returns>
+    public static string ToCamelCase(this string source)
+    {
+        if (!string.IsNullOrEmpty(source) && source.Length > 1)
+        {
+            return char.ToLowerInvariant(source[0]) + source.Substring(1);
         }
 
-        /// <summary>
-        /// Convert a string to double type.
-        /// </summary>
-        /// <param name="source">Source string.</param>
-        /// <returns>Parse result.</returns>
-        public static double ToDouble(this string source)
-        {
-            try
-            {
-                return double.Parse(source, NumberFormatInfo.CurrentInfo);
-            }
-            catch (FormatException)
-            {
-                return double.NaN;
-            }
-        }
-
-        /// <summary>
-        /// Converts an string into a camelCase representation.
-        /// </summary>
-        /// <param name="source">Source string.</param>
-        /// <returns>Camel case string result.</returns>
-        public static string ToCamelCase(this string source)
-        {
-            if (!string.IsNullOrEmpty(source) && source.Length > 1)
-            {
-                return char.ToLowerInvariant(source[0]) + source.Substring(1);
-            }
-
-            return source;
-        }
+        return source;
     }
 }
